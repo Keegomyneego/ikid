5d91ba8663173d3d3fa502599e712f6625bc7383
Keegan Farley <keegomyneego@live.com>
2016-11-03 12:37:45 -0700

    got flip animation working

---
+++ iKid/FlipAnimationController.swift
@@ -0,0 +1,58 @@
+//
+//  FlipAnimationController.swift
+//  iKid
+//
+//  Created by Keegs on 11/3/16.
+//  Copyright © 2016 Keegan Farley. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class FlipAnimationController : NSObject, UIViewControllerAnimatedTransitioning {
+
+    var operation: UINavigationControllerOperation?
+
+    enum Direction {
+        case Forward
+        case Backward
+    }
+
+    private var direction: Direction {
+        return (operation ?? .push) == .push
+            ? .Forward
+            : .Backward
+    }
+
+    private var flipTransition: UIViewAnimationOptions {
+        switch self.direction {
+        case .Forward:  return .transitionFlipFromRight
+        case .Backward: return .transitionFlipFromLeft
+        }
+    }
+
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        switch direction {
+        case .Forward: return 0.5
+        case .Backward: return 0.25
+        }
+    }
+
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let fromView = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!.view!
+        let toView = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!.view!
+
+        print("\n- starting transition")
+        UIView.transition(
+            from: fromView,
+            to: toView,
+            duration: self.transitionDuration(using: transitionContext),
+            options: flipTransition,
+            completion: {
+                finished in
+                print("\n- finished transition")
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            }
+        )
+    }
+}
--- iKid/PageViewController.swift
+++ iKid/PageViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class PageViewController: UIViewController, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let flipAnimationController = FlipAnimationController()
 
+    //--------------------------------------------------------------------------------
+    // UIViewController overrides
+    //--------------------------------------------------------------------------------
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Delegate some navigation controlling logic to this controller
+        navigationController?.delegate = self
     }
 
+    //--------------------------------------------------------------------------------
+    // UINavigationControllerDelegate overrides
+    //--------------------------------------------------------------------------------
+
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+
+        // Set flip direction
+        flipAnimationController.operation = operation
+
+        return flipAnimationController
+    }
 }
 
