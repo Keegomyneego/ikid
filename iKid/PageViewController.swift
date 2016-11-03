698e0221e8d6f78271964017dc46bbe320c1322d
Keegan Farley <keegomyneego@live.com>
2016-11-03 12:37:14 -0700

    got pictures and layout done for knock knock joke

---
+++ iKid/PageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PageViewController.swift
+//  iKid
+//
+//  Created by Keegs on 11/3/16.
+//  Copyright © 2016 Keegan Farley. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIViewController, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
